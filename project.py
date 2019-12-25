c13a908cf5be977f2798b069d5ae672b4f774e13
Fahed Sabellioglu <alijaanm110@gmail.com>
2019-12-25 12:25:29 +0300

    added ObjectDetector class

--- project.py
+++ project.py
@@ -1,9 +1,12 @@
 from SM import SM
 
-class FinalProject(SM):
-    startState = 'nothing'
+class ObstacleDetector(SM):
+    startState = 'nothing'  # you can change this to something else if you want
 
     def getNextValues(self, state, inp):
-        pass
+        if inp < #write ranges here basil:
+            # do specific staff here
+        # continue the same thing here with 5 more elifs.
 
 
+# the objects of Obstacle Detector should run inside a while True.
