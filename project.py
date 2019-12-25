9143e4961ad1659e9bbb6ed6ae029e29d2791d93
basilelkhalifa <basilosama.2000@gmail.com>
2019-12-25 23:58:22 +0300

    States filled in if statements

--- project.py
+++ project.py
@@ -1,12 +1,51 @@
 from SM import SM
+import board
+import digitalio
+import time
+import adafruit_hcsr04
+import pulseio
+
+buzzer = pulseio.PWMOut(board.D8, variable_frequency=True)
+sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D5, echo_pin=board.D0)
+red_led = digitalio.DigitalInOut(board.D1)
+yellow_led= digitalio.DigitalInOut(board.D2)
+green_led = digitalio.DigitalInOut(board.D3)
+red_led.direction = digitalio.Direction.OUTPUT
+yellow_led.direction = digitalio.Direction.OUTPUT
+green_led.direction = digitalio.Direction.OUTPUT
 
 class ObstacleDetector(SM):
-    startState = 'nothing'  # you can change this to something else if you want
+    startState = 'nothing'
 
     def getNextValues(self, state, inp):
-        if inp < #write ranges here basil:
-            # do specific staff here
-        # continue the same thing here with 5 more elifs.
+
+        # First State Discuss with group Members
+
+        if inp > 120 and inp < 200: #Second State
+            print("You are Safe")
+            green_led.value = True
+            red_led.value = False
+            yellow_led.value = False
+        elif inp <= 120 and inp > 50: #Third State
+            print("Slow Down Your Speed")
+            yellow_led.value = True
+            green_led.value = False
+            red_led.value = False
+        elif inp <= 50 and inp > 25:    #Fourth State
+            print("DANGER TOO CLOSE")
+            red_led.value = True
+            green_led.value = False
+            yellow_led.value = False
+        elif inp >= 200: #Discuss with group members Fifth State
+            pass
+        elif inp <= 25: #Discuss with group memebers Sixth State
+            red_led.value = True
+            green_led.value = False
+            yellow_led.value = False
 
 
-# the objects of Obstacle Detector should run inside a while True.
+
+
+CarSensor = ObstacleDetector()
+while True:	#Discuss State and while loop with group memebers
+    CarSensor.getNextValues(0, sonar.distance)
