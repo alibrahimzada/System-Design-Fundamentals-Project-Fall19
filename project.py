0bf6a31b79b96a06a66b522fd1aded974c256642
Ali Jan <alijaanm110@gmail.com>
2019-12-28 14:25:46 +0300

    states were added

--- project.py
+++ project.py
@@ -1,4 +1,3 @@
-from SM import SM
 import board
 import digitalio
 import time
@@ -13,6 +12,22 @@ green_led = digitalio.DigitalInOut(board.D3)
 red_led.direction = digitalio.Direction.OUTPUT
 yellow_led.direction = digitalio.Direction.OUTPUT
 green_led.direction = digitalio.Direction.OUTPUT
+
+
+class SM:
+    def start(self):
+        self.state = self.startState
+    
+    def step(self, inp):
+        (s, o) = self.getNextValues(self.state, inp)
+        self.state = s
+        return o
+    
+    def transduce(self, input):
+        self.start()
+        o = self.step(input)
+        return (self.state, o)
+
 
 class ObstacleDetector(SM):
     startState = 'initialState'
@@ -33,40 +48,57 @@ class ObstacleDetector(SM):
             buzzer.duty_cycle = 0
             return (None, 'Obstacle Detector Device is Working!')
                     
-        elif 120 < inp < 200:                   #Second State
+        elif 120 < inp < 200:
+            red_led.value = False
+            yellow_led.value = False
             green_led.value = True
-            return ('thirdState', 'You are safe')
+            return ('secondState', 'You are safe')
+
+        elif 50 < inp <= 120:
+            green_led.value = False
+            red_led.value = False
+            yellow_led.value = True
+            return ('thirdState', 'Slow Down Your Speed')
+
+        elif 25 < inp <= 50:
+            yellow_led.value = False
+            green_led.value = False
+            red_led.value = True
+            return ('fourthState', 'DANGER TOO CLOSE') 
+
+        elif inp >= 200:
+            red_led.value = False
+            green_led.value = True
+            yellow_led.value = True
+            return ('fifthState', 'Distance Error')
         
-#        if inp > 120 and inp < 200: #Second State
-#            print("You are Safe")
-#            green_led.value = True
-#            red_led.value = False
-#            yellow_led.value = False
-#        elif inp <= 120 and inp > 50: #Third State
-#            print("Slow Down Your Speed")
-#            yellow_led.value = True
-#            green_led.value = False
-#            red_led.value = False
-#        elif inp <= 50 and inp > 25:    #Fourth State
-#            print("DANGER TOO CLOSE")
-#            red_led.value = True
- #           green_led.value = False
-  #          yellow_led.value = False
-   #     elif inp >= 200: #Discuss with group members Fifth State
-    #        pass
-     #   elif inp <= 25: #Discuss with group memebers Sixth State
-      #      red_led.value = True
-       #     green_led.value = False
-        #    yellow_led.value = False
+        elif inp <= 25:
+            green_led.value = False
+            yellow_led.value = False
+            red_led.value = True
+            buzzer.duty_cycle = 2**15
+            return ('finalState', 'The Device Stopped! Calling the emergency services')
 
 
+def getTimeDifference(old_time):
+    return time.time() - old_time
 
-
-CarSensor = ObstacleDetector()
-(s, o) = CarSensor.transduce(sonar.distance)
+obj = ObstacleDetector()
+(s, o) = obj.transduce(sonar.distance)
 print(s, o)
 
-while True:	#Discuss State and while loop with group memebers
-    (s, o) = CarSensor.getNextValues(s, sonar.distance)
-    print(sonar.distance)
-    print(s, o)
+isFinalState = False
+while True:
+    (s, o) = obj.transduce(sonar.distance)
+    print(sonar.distance, s, o)
+    
+    if not isFinalState and s == 'finalState':
+        start = time.time()
+        isFinalState = True
+    elif s != 'finalState':
+        isFinalState = False
+
+    difference = getTimeDifference(start)
+    if s == 'finalState' and difference > 5:
+        print(o)
+        break
