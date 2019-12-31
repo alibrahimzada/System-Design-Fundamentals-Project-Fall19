8e32b4d9c1155c44fe46c6357d2d433e73121973
Ali Jan <alijaanm110@gmail.com>
2019-12-31 19:57:37 +0300

    docstrings added

--- project.py
+++ project.py
@@ -1,9 +1,12 @@
+# importing necessary modules for project
 import board
 import digitalio
 import time
 import adafruit_hcsr04
 import pulseio
 
+
+#Reading drivers for hardwares and introducing them to computer
 buzzer = pulseio.PWMOut(board.D8, variable_frequency=True)
 sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D10, echo_pin=board.D9)
 red_led = digitalio.DigitalInOut(board.D3)
@@ -15,15 +18,58 @@ green_led.direction = digitalio.Direction.OUTPUT
 
 
 class SM:
+    """
+    The State Machine class implements State Machine operations.
+
+    Attributes:
+        None
+
+    Methods:
+        start(self): The method to start the SM and set the start state.
+        step(self, inp): The method to handle output and set new state at each step.
+        transduce(self, input): The method that gets new inputs and give it for self.step.
+    """
+
     def start(self):
+        """
+        The method to start the SM and set the start state.
+
+        Parameters:
+            None
+
+        Returns:
+            None
+        """
+
         self.state = self.startState
 
     def step(self, inp):
+        """
+        The method to handle output and set new state at each step.
+
+        Parameters:
+            inp (float): input value given to state machine at each step
+
+        Returns:
+            o (str): output value from a certain state
+        """
+
         (s, o) = self.getNextValues(self.state, inp)
         self.state = s
         return o
 
     def transduce(self, input):
+        """
+        The method that gets new inputs and give it for self.step.
+
+        Parameters:
+            input (float): The input value given to state machine at each step from sonars.
+
+        Returns:
+            self.state (str): new state of state machine.
+            o (str): output value from a certain state
+        """
+
         if not self.checkState:
             self.start()
             self.checkState = True
@@ -32,16 +78,50 @@ class SM:
 
 
 class ObstacleDetector(SM):
+    """
+    The ObstacleDetector class which implements the states of project.
+
+    Attributes:
+        startState (str): start state of the state machine.
+        checkState (bool): check state in certain points.
+
+    Methods:
+        initialize(self, led): The method which encapsulates a specific functionaility.
+        getNextValues(self, state, inp): The method which handles states based on inputs and current state.
+    """
+
     startState = 'initialState'
     checkState = False
 	
     def initialize(self, led):
+        """
+        The method which encapsulates a specific functionaility.
+
+        Parameters:
+            led (instance): LEDs from circuit
+        
+        Returns:
+            None
+        """
+
         led.value = True
         time.sleep(2)
         led.value = False
         time.sleep(1)
 
     def getNextValues(self, state, inp):
+        """
+        The method which handles states based on inputs and current state.
+
+        Parameters:
+            state (str): The current state of state machine.
+            inp (float): The input value for state machine from sonars.
+        
+        Returns:
+            nextState (str): Next state based on the input value.
+            output (str): Output value based on the input value.
+        """
+
         if state == 'initialState':
             self.initialize(green_led)
             self.initialize(yellow_led)
@@ -82,32 +162,41 @@ class ObstacleDetector(SM):
             yellow_led.value = False
             red_led.value = True
             buzzer.duty_cycle = 2**15
-            return ('finalState', 'The Device Stopped! Calling the emergency services')
+            return ('finalState', '')
 
 
 def getTimeDifference(old_time):
+    """
+    The function which calculates time difference.
+
+    Parameters:
+        old_time (float): The old time in which the interval started.
+
+    Returns:
+        difference (float): The time difference between old_time and current time.
+    """
+
     return time.time() - old_time
 
-obj = ObstacleDetector()
-isFinalState = False
-isInitialState = False
-start = 0
+obj = ObstacleDetector()                                 # An ObstacleDetector instance to start the project
+isFinalState = False                                       # A boolean variable to check if it is finalState
+start = 0                                       # An integer variable to calculate time difference initially
 
-while True:
-    try:
+while True:                                    # An infinite loop to test the Obstacle Detector in real time
+    try:                                        
         sonar_distance = sonar.distance
-    except RuntimeError:
+    except RuntimeError:                     # Catching RuntimeError when waves cannot return back to sensor
         sonar_distance = 200
-    (s, o) = obj.transduce(sonar_distance)
-    print(sonar_distance, s, o)
+    (s, o) = obj.transduce(sonar_distance)       # New state and output based on the given input from sonars
+    print(o)                                                             # Displaying the output on terminal
 
-    if not isFinalState and s == 'finalState':
-        start = time.time()
-        isFinalState = True
-    elif s != 'finalState':
+    if not isFinalState and s == 'finalState':                         # Checking if the SM is in finalState
+        start = time.time()                           # Starting the counter the moment it enters finalState
+        isFinalState = True            # Changing this variable to avoid re-entering and resetting the timer
+    elif s != 'finalState':                            # resetting the timer if the SM leaves the finalState 
         isFinalState = False
 
-    difference = getTimeDifference(start)
-    if s == 'finalState' and difference > 5:
-        print(o)
+    difference = getTimeDifference(start)        # calculating time difference since the SM is in finalState
+    if s == 'finalState' and difference > 5:        # if SM is in finalState more than 5s, the program stops
+        print('The Device Stopped! Calling Emergency Services')   # final output before stopping the program
         break
