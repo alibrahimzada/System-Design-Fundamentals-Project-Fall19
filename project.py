6d4320ee8382abee1484fbeba9172f57082e8538
Ali Jan <alijaanm110@gmail.com>
2020-01-06 12:56:25 +0300

    final+ version

--- project.py
+++ project.py
@@ -20,8 +20,10 @@ green_led.direction = digitalio.Direction.OUTPUT
 class SM:
     """
     The State Machine class implements State Machine operations.
+
     Attributes:
         None
+
     Methods:
         start(self): The method to start the SM and set the start state.
         step(self, inp): The method to handle output and set new state at each step.
@@ -68,8 +70,10 @@ class SM:
     def transduce(self, input):
         """
         The method that gets new inputs and give it for self.step.
+
         Parameters:
             input (float): The input value given to state machine at each step from sonars.
+        
         Returns:
             self.state (str): new state of state machine.
             o (str): output value from a certain state
@@ -82,16 +86,28 @@ class SM:
         return (self.state, o)
 
     def getTimeDiffernce(self, old_time):
+        """
+        The function which calculates time difference.
+
+        Parameters:
+            old_time (float): The old time in which the interval started.
+        
+        Returns:
+            difference (float): The time difference between old_time and current time.
+        """
+
         return time.time() - old_time
 
 
 class ObstacleDetector(SM):
     """
     The ObstacleDetector class which implements the states of project.
+    
     Attributes:
         startState (str): start state of the state machine.
         isInitialState (bool): check if it is initial state
         isFinalState (bool): check if it is final state
+    
     Methods:
         initialize(self, led): The method which encapsulates a specific functionaility.
         getNextValues(self, state, inp): The method which handles states based on inputs and current state.
@@ -104,6 +120,7 @@ class ObstacleDetector(SM):
     def initialize(self, led):
         """
         The method which encapsulates a specific functionaility.
+        
         Parameters:
             led (instance): LEDs from circuit
 
@@ -119,6 +136,7 @@ class ObstacleDetector(SM):
     def getNextValues(self, state, inp):
         """
         The method which handles states based on inputs and current state.
+        
         Parameters:
             state (str): The current state of state machine.
             inp (float): The input value for state machine from sonars.
@@ -167,18 +185,6 @@ class ObstacleDetector(SM):
             return ('closedState', '')
 
 
-def getTimeDifference(old_time):
-    """
-    The function which calculates time difference.
-    Parameters:
-        old_time (float): The old time in which the interval started.
-    Returns:
-        difference (float): The time difference between old_time and current time.
-    """
-
-    return time.time() - old_time
-
-
 obj = ObstacleDetector()                                 # An ObstacleDetector instance to start the project
 while True:                                    # An infinite loop to test the Obstacle Detector in real time
     try:
@@ -186,7 +192,7 @@ while True:                                    # An infinite loop to test the Ob
     except RuntimeError:                     # Catching RuntimeError when waves cannot return back to sensor
         sonar_distance = 200
     (s, o) = obj.transduce(sonar_distance)       # New state and output based on the given input from sonars
-    print(sonar_distance, s, o)                                          # Displaying the output on terminal
+    print(o)                                                             # Displaying the output on terminal
     if s == 'finalState':                                              # Checking if the SM is in finalState
         print('The Device Stopped! Calling Emergency Services')   # final output before stopping the program
         green_led.value = False
